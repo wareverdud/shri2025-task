590c799409ee0a0c4a19f51d22c73c779849ef10
wareverdud <71446364+wareverdud@users.noreply.github.com>
2025-07-03 21:45:16 +0300

    js refactor and font

--- src/index.jsx
+++ src/index.jsx
@@ -2,61 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../styles.min.css";
 
-function Header() {
-  let [expanded, setExpanded] = React.useState(false);
-  let [toggled, setToggled] = React.useState(false);
-
-  const onClick = () => {
-    if (!toggled) {
-      setToggled(true);
-    }
-
-    setExpanded(!expanded);
-  };
-
-  return (
-    <header className="header">
-      <a href="/" className="header__logo" aria-label="Яндекс.Дом"></a>
-      <button
-        className="header__menu"
-        aria-expanded={expanded ? "true" : "false"}
-        onClick={onClick}
-      >
-        <span className="header__menu-text a11y-hidden">
-          {expanded ? "Закрыть меню" : "Открыть меню"}
-        </span>
-      </button>
-      <ul
-        className={
-          "header__links" +
-          (expanded ? " header__links_opened" : "") +
-          (toggled ? " header__links-toggled" : "")
-        }
-      >
-        <li className="header__item">
-          <a
-            className="header__link header__link_current"
-            href="/"
-            aria-current="page"
-          >
-            Сводка
-          </a>
-        </li>
-        <li className="header__item">
-          <a className="header__link" href="/devices">
-            Устройства
-          </a>
-        </li>
-        <li className="header__item">
-          <a className="header__link" href="/scripts">
-            Сценарии
-          </a>
-        </li>
-      </ul>
-    </header>
-  );
-}
-
 function Event(props) {
   const ref = React.useRef();
 
@@ -428,10 +373,4 @@ function Main() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(
-  <>
-    <Header />
-    <Main />
-  </>
-);
+ReactDOM.createRoot(document.getElementById("app")).render(<Main />);
